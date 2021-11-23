363acf4a145abe49ea6e24fbef796fdfbca36626
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2021-11-23 02:23:48 +0100

    Added Employee and WorkingHours entity

---
+++ FoodStoreMarket.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using FoodStoreMarket.Domain.Common;
+using FoodStoreMarket.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStoreMarket.Domain.Entities
+{
+    public class Employee : AuditableEntity
+    {
+        public PersonName PersonName { get; set; }
+        public Adres Adres { get; set; }
+        public List<WorkingHours> WorkingHours { get; set; } = new List<WorkingHours>();
+    }
+}
---
+++ FoodStoreMarket.Domain/Entities/WorkingHours.cs
@@ -0,0 +1,18 @@
+﻿using FoodStoreMarket.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStoreMarket.Domain.Entities
+{
+    public class WorkingHours : AuditableEntity
+    {
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime DateOfStartWork { get; set; }
+        public DateTime DateOfEndWork { get; set; }
+        public decimal? WorkHours { get; set; }
+    }
+}
