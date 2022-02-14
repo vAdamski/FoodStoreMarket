bbb15826fd3269d1fdeff41736c767542b575cc3
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-02-14 13:11:06 +0100

    Create restaurant update

---
+++ FoodStoreMarket.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStoreMarket.Application.Restaurants.Commands.CreateRestaurant
+{
+    public class CreateRestaurantCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string FlatNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
