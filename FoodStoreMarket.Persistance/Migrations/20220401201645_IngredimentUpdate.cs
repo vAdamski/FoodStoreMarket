a322e71eeb62f2190f66c792b25e0511378d4443
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-04-02 00:07:23 +0200

    ud

---
+++ FoodStoreMarket.Persistance/Migrations/20220401201645_IngredimentUpdate.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodStoreMarket.Persistance.Migrations
+{
+    public partial class IngredimentUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IndegrimentProductSpecification");
+
+            migrationBuilder.DropTable(
+                name: "Indegriments");
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false),
+                    InactivatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Inactivated = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientProductSpecification",
+                columns: table => new
+                {
+                    IngredientsId = table.Column<int>(type: "int", nullable: false),
+                    ProductSpecificationsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientProductSpecification", x => new { x.IngredientsId, x.ProductSpecificationsId });
+                    table.ForeignKey(
+                        name: "FK_IngredientProductSpecification_Ingredients_IngredientsId",
+                        column: x => x.IngredientsId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientProductSpecification_ProductSpecifications_ProductSpecificationsId",
+                        column: x => x.ProductSpecificationsId,
+                        principalTable: "ProductSpecifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientProductSpecification_ProductSpecificationsId",
+                table: "IngredientProductSpecification",
+                column: "ProductSpecificationsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientProductSpecification");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.CreateTable(
+                name: "Indegriments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Inactivated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    InactivatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Modified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StatusId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Indegriments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IndegrimentProductSpecification",
+                columns: table => new
+                {
+                    IndegrimentsId = table.Column<int>(type: "int", nullable: false),
+                    ProductSpecificationsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IndegrimentProductSpecification", x => new { x.IndegrimentsId, x.ProductSpecificationsId });
+                    table.ForeignKey(
+                        name: "FK_IndegrimentProductSpecification_Indegriments_IndegrimentsId",
+                        column: x => x.IndegrimentsId,
+                        principalTable: "Indegriments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IndegrimentProductSpecification_ProductSpecifications_ProductSpecificationsId",
+                        column: x => x.ProductSpecificationsId,
+                        principalTable: "ProductSpecifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IndegrimentProductSpecification_ProductSpecificationsId",
+                table: "IndegrimentProductSpecification",
+                column: "ProductSpecificationsId");
+        }
+    }
+}
--- FoodStoreMarket/Controllers/IngredientsController.cs
+++ FoodStoreMarket/Controllers/IngredientsController.cs
@@ -1,16 +1,101 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Cors;
-using System;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
 namespace FoodStoreMarket.Api.Controllers
 {
 	[Route("api/ingredients")]
 	[EnableCors("MyAllowSecificOrigins")]
 	public class IngredientsController : BaseController
 	{
-		public IngredientsController()
-		{
+        /// <summary>
+        /// Get igredient by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetByIdAsync(int id)
+        {
+            return Ok();
+        }
 
-		}
-	}
+        /// <summary>
+        /// Get all ingredients in restaurant
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetAllAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get all ingredients for product
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> GetAllForProductAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Add ingredient
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public async Task<ActionResult<int>> PostAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Update ingredient
+        /// </summary>
+        /// <returns></returns>
+        [HttpPatch]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> PatchAsync()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Delete ingredient by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            return Ok();
+        }
+    }
 }
 
