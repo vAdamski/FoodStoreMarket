594a075a7b432e7dca1c50bf20d1a863e2c4c066
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-04-27 23:18:19 +0200

    OnDelete

---
+++ FoodStoreMarket.Persistance/Migrations/20220424232155_RestaurantOnDeleteI.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodStoreMarket.Persistance.Migrations
+{
+    public partial class RestaurantOnDeleteI : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Menus_Restaurants_RestaurantId",
+                table: "Menus");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantSpecifications_Restaurants_RestaurantId",
+                table: "RestaurantSpecifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Menus_Restaurants_RestaurantId",
+                table: "Menus",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantSpecifications_Restaurants_RestaurantId",
+                table: "RestaurantSpecifications",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Menus_Restaurants_RestaurantId",
+                table: "Menus");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantSpecifications_Restaurants_RestaurantId",
+                table: "RestaurantSpecifications");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Menus_Restaurants_RestaurantId",
+                table: "Menus",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantSpecifications_Restaurants_RestaurantId",
+                table: "RestaurantSpecifications",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
