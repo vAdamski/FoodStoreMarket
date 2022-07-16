0a625ad6c6629220e7e2b82a1a8580b95dc3fda9
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-07-16 02:17:28 +0200

    Size CRUD added

---
+++ FoodStoreMarket.Application/Sizes/Commands/AddNewSize/AddNewSizeHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FoodStoreMarket.Application.Interfaces;
+using FoodStoreMarket.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodStoreMarket.Application.Sizes.Commands.AddNewSize;
+
+public class AddNewSizeHandler : IRequestHandler<AddNewSizeCommand, int>
+{
+    private IFoodStoreMarketDbContext _context;
+    private IMapper _mapper;
+
+    public AddNewSizeHandler(IFoodStoreMarketDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<int> Handle(AddNewSizeCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var menuIsValid = await _context.Menus.Where(x => x.Id == request.MenuId && x.StatusId == 1)
+                .FirstOrDefaultAsync(cancellationToken);
+            var productTypeIsValid = await _context.ProductTypes
+                .Where(x => x.Id == request.ProductTypeId && x.StatusId == 1).FirstOrDefaultAsync(cancellationToken);
+
+            if (menuIsValid == null)
+            {
+                throw new Exception("Menu not exist!");
+            }
+
+            if (productTypeIsValid == null)
+            {
+                throw new Exception("Product type not exist!");
+            }
+            
+            var sizeToAdd = _mapper.Map<Size>(request);
+
+            await _context.Sizes.AddAsync(sizeToAdd, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return sizeToAdd.Id;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
