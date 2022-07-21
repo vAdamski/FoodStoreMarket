d5a0a0c20390b065d24b1b271e7c72aa7c2a2ef5
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-07-21 12:07:04 +0200

    middleware

--- FoodStoreMarket.Application/DependencyInjection.cs
+++ FoodStoreMarket.Application/DependencyInjection.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using FoodStoreMarket.Application.Common.Behaviours;
+using MediatR.Pipeline;
 
 namespace FoodStoreMarket.Application
 {
@@ -16,6 +18,9 @@ namespace FoodStoreMarket.Application
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
+
+            services.AddTransient(typeof(IRequestPreProcessor<>), typeof(LoggingBehaviour<>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
             return services;
         }
     }
