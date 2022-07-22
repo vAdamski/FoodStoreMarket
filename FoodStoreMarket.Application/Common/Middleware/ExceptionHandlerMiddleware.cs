2c1d976df522833617980922535eaa78dd4a58f6
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-07-22 10:33:42 +0200

    Middleware

---
+++ FoodStoreMarket.Application/Common/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace FoodStoreMarket.Application.Common.Middleware;
+
+public class ExceptionHandlerMiddleware
+{
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var code = HttpStatusCode.InternalServerError;
+
+        var result = string.Empty;
+
+        switch (exception)
+        {
+            case ValidationException validationException:
+                code = HttpStatusCode.BadRequest;
+                result = JsonConvert.SerializeObject(validationException.Failures);
+                break;
+            case BadRequestException _:
+                code = HttpStatusCode.BadRequest;
+                break;
+            case MissingRequestSecurityDeclarationException _:
+                code = HttpStatusCode.InternalServerError;
+                break;
+        }
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+
+        if (result == string.Empty)
+        {
+            result = JsonConvert.SerializeObject(new { error = exception.Message });
+        }
+
+        return context.Response.WriteAsync(result);
+    }
+}
