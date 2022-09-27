7db9cbd72fc0f095e3a381e40ef1148221f9d952
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-09-27 02:42:05 +0200

    Added first query test for restaurant

---
+++ Application.UnitTests/Common/QueryTestFixtures.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using FoodStoreMarket.Application.Common.Mappings;
+using FoodStoreMarket.Persistance;
+
+namespace Application.UnitTests.Common;
+
+public class QueryTestFixtures : IDisposable
+{
+    public FoodStoreMarketDbContext Context { get; private set; }
+    public IMapper Mapper { get; private set; }
+
+    public QueryTestFixtures()
+    {
+        Context = FoodStoreMarketDbContextFactory.Create().Object;
+        
+        var configurationProvider = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<MappingProfile>();
+        });
+
+        Mapper = configurationProvider.CreateMapper();
+    }
+
+    public void Dispose()
+    {
+        FoodStoreMarketDbContextFactory.Destory(Context);
+    }
+    
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<QueryTestFixtures> { }
+}
---
+++ Application.UnitTests/Restaurants/Queries/GetRestaurantDetail/GetRestaurantDetailQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.UnitTests.Common;
+using AutoMapper;
+using FoodStoreMarket.Application.Restaurants.Queries.GetRestaurantDetail;
+using FoodStoreMarket.Persistance;
+using Shouldly;
+
+namespace Application.UnitTests.Restaurants.Queries.GetRestaurantDetail;
+
+[Collection("QueryCollection")]
+public class GetRestaurantDetailQueryHandlerTests
+{
+    private readonly FoodStoreMarketDbContext _context;
+    private readonly IMapper _mapper;
+    public GetRestaurantDetailQueryHandlerTests(QueryTestFixtures fixtures)
+    {
+        _context = fixtures.Context;
+        _mapper = fixtures.Mapper;
+    }
+
+    [Fact]
+    public async Task CanGetRestaurantById()
+    {
+        var handler = new GetRestaurantDetailQueryHandler(_context, _mapper);
+        var restaurantId = 1;
+
+        var result = await handler.Handle(new GetRestaurantDetailQuery { RestaurantId = restaurantId },
+            CancellationToken.None);
+
+        result.ShouldBeOfType<RestaurantDetailVm>();
+        result.Name.ShouldBe("Pizzeria #1");
+    }
+}
