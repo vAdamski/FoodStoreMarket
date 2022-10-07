b001960c07e95d40524e9d227c4ebce0093844b5
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-10-07 21:08:16 +0200

    Restaurant CRUD update

--- Application.UnitTests/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantHandlerTests.cs
+++ Application.UnitTests/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantHandlerTests.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using Application.UnitTests.Common;
+using FoodStoreMarket.Application.Restaurants.Commands.DeleteRestaurant;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace Application.UnitTests.Restaurants.Commands.DeleteRestaurant;
+
+public class DeleteRestaurantHandlerTests : CommandTestBase, IClassFixture<CommandTestFixture>
+{
+    private readonly DeleteRestaurantCommandHandler _handler;
+
+    public DeleteRestaurantHandlerTests(CommandTestFixture fixture) : base()
+    {
+        _handler = new DeleteRestaurantCommandHandler(_context);
+    }
+
+    [Fact]
+    public async Task Handle_GivenValidRequest_RestaurantShouldBeSoftDeleted()
+    {
+        var restaurantIdToDelete = 1;
+        
+        var command = new DeleteRestaurantCommand()
+        {
+            IdRestaurantToDelete = restaurantIdToDelete
+        };
+
+        _handler.Handle(command, CancellationToken.None);
+
+        var resultRestaurant =
+            await _context.Restaurants.FirstOrDefaultAsync(x => x.Id == restaurantIdToDelete && x.StatusId == 0);
+        
+        var resultRestaurantSpecification =
+            await _context.Restaurants.FirstOrDefaultAsync(x => x.Id == restaurantIdToDelete && x.StatusId == 0);
+        
+        var result =
+            await _context.Restaurants.FirstOrDefaultAsync(x => x.Id == restaurantIdToDelete && x.StatusId == 0);
+    }
+}
--- FoodStoreMarket.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
+++ FoodStoreMarket.Application/Restaurants/Commands/DeleteRestaurant/DeleteRestaurantCommandHandler.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using FoodStoreMarket.Domain.Entities;
 
 namespace FoodStoreMarket.Application.Restaurants.Commands.DeleteRestaurant
 {
@@ -20,23 +21,105 @@ namespace FoodStoreMarket.Application.Restaurants.Commands.DeleteRestaurant
 
         public async Task<Unit> Handle(DeleteRestaurantCommand request, CancellationToken cancellationToken)
         {
-            var restaurantToDelete = await _context.Restaurants
-                .Where(r => r.Id == request.IdRestaurantToDelete)
-                .FirstOrDefaultAsync();
+            var restaurantData = await GetAllRestaurantData(request.IdRestaurantToDelete, cancellationToken);
 
-            restaurantToDelete.Menu = await _context.Menus
-                .Where(m => m.RestaurantId == restaurantToDelete.Id)
-                .FirstOrDefaultAsync();
-            
-            restaurantToDelete.RestaurantSpecification = await _context.RestaurantSpecifications
-                .Where(rs => rs.RestaurantId == restaurantToDelete.Id)
-                .FirstOrDefaultAsync();
-
-            _context.Restaurants.Remove(restaurantToDelete);
-
-            await _context.SaveChangesAsync(cancellationToken);
+            DeleteRestaurantData(restaurantData);
 
             return Unit.Value;
+        }
+
+        private async Task<Restaurant> GetAllRestaurantData(int restaurantId, CancellationToken cancellationToken)
+        {
+            var data = await _context.Restaurants
+                .Where(r => r.Id == restaurantId)
+                .Include(r => r.Menu)
+                .ThenInclude(m => m.Products)
+                .ThenInclude(p => p.ProductSpecification)
+                .ThenInclude(ps => ps.ProductSizeSpecifications)
+                .ThenInclude(pss => pss.Size)
+                .Include(r => r.Menu)
+                .ThenInclude(m => m.Ingredients)
+                .Include(r => r.Menu)
+                .ThenInclude(m => m.ProductTypes)
+                .Include(r => r.RestaurantSpecification)
+                .ThenInclude(rs => rs.OpeningClosingSpecification)
+                .ThenInclude(ocs => ocs.OpeningClosingHours)
+                .Include(r => r.RestaurantSpecification)
+                .ThenInclude(rs => rs.Employees)
+                .ThenInclude(e => e.WorkingHours)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            return data;
+        }
+
+        private void DeleteRestaurantData(Restaurant restaurant)
+        {
+            RestaurantDelete(restaurant);
+            MenuDelete(restaurant.Menu);
+            ProductsDelete(restaurant.Menu.Products);
+
+            var productsSpec = restaurant.Menu.Products.Select(x => x.ProductSpecification).ToList();
+            ProductsSpecDelete(productsSpec);
+
+            var ingredients = restaurant.Menu.Ingredients;
+            IngredientsDelete(ingredients);
+
+            var productTypes = restaurant.Menu.ProductTypes;
+            ProductTypesDelete(productTypes);
+
+            var sizes = restaurant.Menu.Sizes;
+            SizesDelete(sizes);
+
+            var productSizeSpec = new List<ProductSizeSpecification>();
+
+            sizes.ForEach(s =>
+            {
+                productSizeSpec.AddRange(s.ProductSizeSpecifications);
+            });
+
+            ProductSizeSpecificationDelete(productSizeSpec);
+
+
+        }
+
+        private void RestaurantDelete(Restaurant restaurant)
+        {
+            _context.Restaurants.Remove(restaurant);
+        }
+
+        private void MenuDelete(Menu menu)
+        {
+            _context.Menus.Remove(menu);
+        }
+
+        private void ProductsDelete(List<Product> products)
+        {
+            _context.Products.RemoveRange(products);
+        }
+
+        private void ProductsSpecDelete(List<ProductSpecification> productsSpec)
+        {
+            _context.ProductSpecifications.RemoveRange(productsSpec);
+        }
+
+        private void IngredientsDelete(List<Ingredient> ingredients)
+        {
+            _context.Ingredients.RemoveRange(ingredients);
+        }
+
+        private void ProductTypesDelete(List<ProductType> productTypes)
+        {
+            _context.ProductTypes.RemoveRange(productTypes);
+        }
+
+        private void SizesDelete(List<Size> sizes)
+        {
+            _context.Sizes.RemoveRange(sizes);
+        }
+
+        private void ProductSizeSpecificationDelete(List<ProductSizeSpecification> productSizeSpecifications)
+        {
+            _context.ProductSizeSpecifications.RemoveRange(productSizeSpecifications);
         }
     }
 }
