079dbf446653e58001a230f6e54905a9d521a7c0
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-10-16 01:23:11 +0200

    InvalidRequestException update, added description to describe whats happend

--- FoodStoreMarket.Domain/Exceptions/InvalidRequestException.cs
+++ FoodStoreMarket.Domain/Exceptions/InvalidRequestException.cs
@@ -4,8 +4,8 @@ namespace FoodStoreMarket.Domain.Exceptions;
 
 public class InvalidRequestException: Exception
 {
-    public InvalidRequestException(Type objectType,string propertyName) 
-        : base($"{objectType.ToString()} found problem with {propertyName}", new Exception())
+    public InvalidRequestException(Type objectType,string propertyName, string description = "") 
+        : base($"Found problem with {objectType.ToString()} {propertyName}. Info = {description}", new Exception())
     {
         
     }
