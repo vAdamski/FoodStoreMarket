c23ed185ca103b396cc83cba1ba4adfc07eeccd2
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-12-19 19:09:42 +0100

    Update

--- FoodStoreMarket/Client/Models/RestaurantDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FoodStoreMarket.Client.Models
-{
-    public class RestaurantDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
