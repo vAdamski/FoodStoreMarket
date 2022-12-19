0e179bea45d953b04d0e1baefb00c0fa17a553eb
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-12-19 19:09:58 +0100

    Update

---
+++ FoodStoreMarket/Shared/Models/Restaurant/Commands/CreateRestaurant/RestaurantsForm.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodStoreMarket.Shared.Models.Restaurant.Commands.CreateRestaurant
+{
+    public class RestaurantsForm
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string FlatNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ FoodStoreMarket/Shared/Models/Restaurant/Queries/GetAllRestaurants/RestaurantDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodStoreMarket.Shared.Models.Restaurant.Queries.GetAllRestaurants
+{
+    public class RestaurantDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
